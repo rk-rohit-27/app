f846edb71004e73754be7209db00c6c6b44d8f96
rk-rohit-27 <rk27rohit@gmail.com>
2025-06-04 12:37:52 +0530

    fix

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import './globals.css'
+
+const index = () => {
+  return (
+    <View className='flex-1 bg-red-400 flex items-center justify-center'>
+      <Text className='text-2xl'>Welcome</Text>
+    </View>
+  )
+}
+
+export default index
